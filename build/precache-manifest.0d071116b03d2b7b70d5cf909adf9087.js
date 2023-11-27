e25b5e8ebad1ddf8993ec42d471823864c9bfdd4
BojjchenkoDmitro87 <150932284+BojjchenkoDmitro87@users.noreply.github.com>
2023-11-27 14:40:13 +0200

    Add files via upload

---
+++ build/precache-manifest.0d071116b03d2b7b70d5cf909adf9087.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8641ecbb62f461eb50a3",
+    "url": "/static/css/main.e97196da.chunk.css"
+  },
+  {
+    "revision": "8641ecbb62f461eb50a3",
+    "url": "/static/js/main.8641ecbb.chunk.js"
+  },
+  {
+    "revision": "be12b7a4e070c991d465",
+    "url": "/static/js/1.be12b7a4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eb5229e7ea72f37d58ca81860af944a",
+    "url": "/static/media/click.9eb5229e.mp3"
+  },
+  {
+    "revision": "772a3047c19a43a4298b48533785a8f7",
+    "url": "/static/media/win.772a3047.mp3"
+  },
+  {
+    "revision": "e9c53d8c945838538b31faf0edd70745",
+    "url": "/static/media/PTSans-Bold.e9c53d8c.woff"
+  },
+  {
+    "revision": "3f40ccfd35207c3629cba01c8db1082a",
+    "url": "/static/media/PTSans-Regular.3f40ccfd.woff"
+  },
+  {
+    "revision": "4099acba756140ddcc2a58f22d5040e9",
+    "url": "/static/media/bg.4099acba.jpg"
+  },
+  {
+    "revision": "be4f4b9e57d9b22c2acd5234ddc14960",
+    "url": "/index.html"
+  }
+];
